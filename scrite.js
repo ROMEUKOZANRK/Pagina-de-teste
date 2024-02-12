33bb53e140650da064c90dcb6dcf0a3e1721126e
ROMEUKOZANRK <154615367+ROMEUKOZANRK@users.noreply.github.com>
2024-02-12 18:33:21 +0100

    Criadas CLINICA DA MULHER index.html

---
+++ scrite.js
@@ -0,0 +1,10 @@
+ const doutora = document.getElementById('doutora');
+
+ doutora.addEventListener('mouseover', function(){
+    doutora.src = "img/doutora-1.png";
+ });
+  
+ doutora.addEventListener('mouseout', function(){
+    doutora.src = "img/doutora-3.png";
+ });
+  
